d37a18f4a6602c92e42730e72abdc6200f79de44
satvikkapur-dev <satvikkapur@gmail.com>
2026-07-27 23:14:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,14 +11,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);nfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
-
-export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
